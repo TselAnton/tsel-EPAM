bb0ed5f3d439384c87e0eb3cdfbd2086a2d9f205
Tsel Anton <antontsel@yandex.ru>
2019-05-12 01:53:19 +0400

    Altered DB

--- 9 Lesson (DataBase)/create_tables.sql
+++ 9 Lesson (DataBase)/create_tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE if not exists public."role"
 (
 	id SERIAL NOT NULL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL,
-	qty INTEGER
+	name VARCHAR(30) NOT NULL UNIQUE,
+	qty INTEGER DEFAULT = 0
 );
 
 ALTER TABLE public."role" OWNER to postgres;
@@ -10,7 +10,7 @@ ALTER TABLE public."role" OWNER to postgres;
 CREATE TABLE if not exists public."order_status"
 (
 	id SERIAL NOT NULL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL
+	name VARCHAR(30) NOT NULL UNIQUE
 );
 
 ALTER TABLE public."order_status" OWNER to postgres;
@@ -18,7 +18,7 @@ ALTER TABLE public."order_status" OWNER to postgres;
 CREATE TABLE if not exists public."product_category"
 (
 	id SERIAL NOT NULL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL
+	name VARCHAR(30) NOT NULL UNIQUE
 );
 
 ALTER TABLE public."product_category" OWNER to postgres;
@@ -27,17 +27,17 @@ CREATE TABLE if not exists public."user"
 (
 	id SERIAL NOT NULL PRIMARY KEY,
 	fio VARCHAR(90),
-	login VARCHAR(30) NOT NULL,
-	password VARCHAR(30) NOT NULL,
-	phone VARCHAR(11) NOT NULL,
-	email VARCHAR(30) NOT NULL,
+	login VARCHAR(30) NOT NULL UNIQUE,
+	password VARCHAR(30) NOT NULL UNIQUE,
+	phone VARCHAR(11) NOT NULL UNIQUE,
+	email VARCHAR(30) NOT NULL UNIQUE,
 	city VARCHAR(30) NOT NULL,
 	street VARCHAR(30),
-	house VARCHAR(3),
+	house VARCHAR(10),
+	apartment VARCHAR(10),
 	registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	role_id INTEGER REFERENCES public."role" (id),
-	qty_orders INTEGER,
-	UNIQUE(login, phone, email)
+	qty_orders INTEGER DEFAULT = 0
 );
 
 ALTER TABLE public."user" OWNER to postgres;
@@ -45,14 +45,13 @@ ALTER TABLE public."user" OWNER to postgres;
 CREATE TABLE if not exists public."product"
 (
 	id SERIAL NOT NULL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL,
+	name VARCHAR(30) NOT NULL UNIQUE,
 	category_id INTEGER REFERENCES public."product_category" (id),
 	brand VARCHAR(30) NOT NULL,
 	price INTEGER NOT NULL,
-	qty INTEGER DEFAULT 0,
-	discount INTEGER DEFAULT 0,
+	qty INTEGER DEFAULT = 0,
+	discount INTEGER DEFAULT = 0,
 	description text NOT NULL,
-	UNIQUE(name)
 );
 
 ALTER TABLE public."product" OWNER to postgres;
@@ -63,7 +62,7 @@ CREATE TABLE if not exists public."order"
 	user_id INTEGER REFERENCES public."user" (id),
 	date TIMESTAMP WITHOUT TIME ZONE 
 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	status_id INTEGER REFERENCES public."order_status" (id)	DEFAULT 1
+	status_id INTEGER REFERENCES public."order_status" (id)
 );
 
 ALTER TABLE public."order" OWNER to postgres;
